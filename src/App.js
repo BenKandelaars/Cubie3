947ba40c479b7b93da3f81aae2dc8b5a8c97a511
Ben <dutchpossum@hotmail.com>
2017-10-31 18:40:20 +0000

    Basic screen display's cube

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import Interface from './Components/Interface/Interface';
+import CubieSpace from './Components/Canvas/CubieSpace';
 
 class App extends Component {
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <div>
-        <h1>Welcome to React</h1>
+        <h1>Cubie Header</h1>
+        <Interface />
+        <CubieSpace
+          height={500}
+          width={500}
+        />
       </div>
     );
   }
